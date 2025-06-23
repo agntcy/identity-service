46cc8c79246e9f6c9e6d4793d7e221d33ebef72d
jdiaconu <jdiaconu@cisco.com>
2025-06-23 16:56:58 +0200

    feat: Rename some fields
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

--- backend/internal/core/policy/types/types.go
+++ backend/internal/core/policy/types/types.go
@@ -3,6 +3,21 @@
 
 package types
 
+// Identity Platform Policy Task
+type Task struct {
+	// A unique identifier for the Task.
+	ID string `json:"id,omitempty" protobuf:"bytes,1,opt,name=id"`
+
+	// A human-readable name for the Task.
+	Name *string `json:"name,omitempty" protobuf:"bytes,4,opt,name=name"`
+
+	// An application ID for the Task.
+	AppID string `json:"app_id,omitempty" protobuf:"bytes,3,opt,name=app_id"`
+
+	// A tool name for the Task.
+	ToolName string `json:"tool_name,omitempty" protobuf:"bytes,2,opt,name=tool_name"`
+}
+
 // Identity Platform Policy Rule
 type Rule struct {
 	// A unique identifier for the App.
@@ -15,13 +30,10 @@ type Rule struct {
 	Description *string `json:"description,omitempty" protobuf:"bytes,5,opt,name=description"`
 
 	// The requester application that this Rule applies to.
-	RequesterID string `json:"requester_id,omitempty" protobuf:"bytes,2,opt,name=requester_id"`
+	AssignedTo string `json:"assigned_to,omitempty" protobuf:"bytes,2,opt,name=assigned_to"`
 
-	// The target application that this Rule applies to.
-	TargetIDs []string `json:"target_ids,omitempty" protobuf:"bytes,3,opt,name=target_ids"`
-
-	// The allowed tools for this Rule.
-	AllowedToolNames []string `json:"allowed_tool_names,omitempty" protobuf:"bytes,7,rep,name=allowed_tool_names"`
+	// The tasks that this Rule applies to.
+	Tasks []Task `json:"tasks,omitempty" protobuf:"bytes,3,rep,name=tasks"`
 
 	// Need User Approval for this Rule.
 	NeedsApproval bool `json:"needs_approval,omitempty" protobuf:"varint,8,opt,name=needs_approval"`
