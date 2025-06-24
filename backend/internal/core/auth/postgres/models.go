2fa175b9664458a485d2c91bd24847fa9e8db402
jdiaconu <jdiaconu@cisco.com>
2025-06-24 16:15:07 +0200

    feat: Implement core auth
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

---
+++ backend/internal/core/auth/postgres/models.go
@@ -0,0 +1,47 @@
+// Copyright 2025 AGNTCY Contributors (https://github.com/agntcy)
+// SPDX-License-Identifier: Apache-2.0
+
+package postgres
+
+import (
+	types "github.com/agntcy/identity-platform/internal/core/auth/types"
+)
+
+type Session struct {
+	ID         string `gorm:"primarykey"`
+	CreatedAt  int64  `gorm:"autoCreateTime"`
+	ExpiresAt  *int64
+	OwnerAppID string `gorm:"references:OwnerAppID"`
+	AppID      string `gorm:"references:AppID"`
+	ToolName   *string
+	UserID     *string
+	TokenID    *string
+	Code       *string
+}
+
+func (i *Session) ToCoreType() *types.Session {
+	return &types.Session{
+		ID:         i.ID,
+		OwnerAppID: i.OwnerAppID,
+		AppID:      i.AppID,
+		ToolName:   i.ToolName,
+		UserID:     i.UserID,
+		TokenID:    i.TokenID,
+		Code:       i.Code,
+		CreatedAt:  i.CreatedAt,
+		ExpiresAt:  i.ExpiresAt,
+	}
+}
+
+func newSessionModel(src *types.Session) *Session {
+	return &Session{
+		ID:         src.ID,
+		OwnerAppID: src.OwnerAppID,
+		AppID:      src.AppID,
+		ToolName:   src.ToolName,
+		UserID:     src.UserID,
+		TokenID:    src.TokenID,
+		Code:       src.Code,
+		ExpiresAt:  src.ExpiresAt,
+	}
+}
---
+++ backend/internal/core/auth/postgres/repository.go
@@ -0,0 +1,90 @@
+// Copyright 2025 AGNTCY Contributors (https://github.com/agntcy)
+// SPDX-License-Settingsentifier: Apache-2.0
+
+package postgres
+
+import (
+	"context"
+
+	sessioncore "github.com/agntcy/identity-platform/internal/core/auth"
+	"github.com/agntcy/identity-platform/internal/core/auth/types"
+	"github.com/agntcy/identity-platform/internal/pkg/errutil"
+	"github.com/agntcy/identity-platform/internal/pkg/ptrutil"
+	"github.com/agntcy/identity-platform/internal/pkg/strutil"
+	"github.com/agntcy/identity-platform/pkg/db"
+)
+
+const (
+	codeLength = 128
+)
+
+type postgresRepository struct {
+	dbContext db.Context
+}
+
+func NewRepository(dbContext db.Context) sessioncore.Repository {
+	return &postgresRepository{
+		dbContext: dbContext,
+	}
+}
+
+func (r *postgresRepository) Create(
+	ctx context.Context,
+	session *types.Session,
+) (*types.Session, error) {
+	model := newSessionModel(session)
+
+	// Generate a new code
+	model.Code = ptrutil.Ptr(strutil.Random(codeLength))
+
+	result := r.dbContext.Client().Create(model)
+	if result.Error != nil {
+		return nil, errutil.Err(
+			result.Error, "there was an error creating the session",
+		)
+	}
+
+	return model.ToCoreType(), nil
+}
+
+func (r *postgresRepository) GetByCode(ctx context.Context, code string) (*types.Session, error) {
+	model := &Session{}
+
+	result := r.dbContext.Client().Where("code = ?", code).First(model)
+	if result.Error != nil {
+		return nil, errutil.Err(
+			result.Error, "there was an error retrieving the session by code",
+		)
+	}
+
+	return model.ToCoreType(), nil
+}
+
+func (r *postgresRepository) GetByTokenID(
+	ctx context.Context,
+	tokenID string,
+) (*types.Session, error) {
+	model := &Session{}
+
+	result := r.dbContext.Client().Where("token_id = ?", tokenID).First(model)
+	if result.Error != nil {
+		return nil, errutil.Err(
+			result.Error, "there was an error retrieving the session by token ID",
+		)
+	}
+
+	return model.ToCoreType(), nil
+}
+
+func (r *postgresRepository) Update(ctx context.Context, session *types.Session) error {
+	model := newSessionModel(session)
+
+	result := r.dbContext.Client().Save(model)
+	if result.Error != nil {
+		return errutil.Err(
+			result.Error, "there was an error updating the session",
+		)
+	}
+
+	return nil
+}
