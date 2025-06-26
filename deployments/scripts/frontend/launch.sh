780bf58f52641d6f449faa0987fdb8101cb6e789
Rafael Silva (rafaelsi) <rafaelsi@cisco.com>
2025-06-26 09:29:52 +0100

    feat: add docker ui

---
+++ deployments/scripts/frontend/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Copyright 2025 AGNTCY Contributors (https://github.com/agntcy)
+# SPDX-License-Identifier: Apache-2.0
+
+# If .env exists in the frontend directory, use it
+# If not, create an env with defaults
+./deployments/scripts/frontend/env_setup.sh
+
+# Check if dev option is set
+compose_file="./deployments/docker-compose/frontend/docker-compose.yml"
+
+docker compose -f "$compose_file" build --no-cache
+docker compose -f "$compose_file" up -d
