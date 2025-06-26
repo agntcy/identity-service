647acbca6083f0c79cce0638be0be2f846cdc189
jdiaconu <jdiaconu@cisco.com>
2025-06-26 14:25:52 +0200

    feat: Update ui headers
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

--- frontend/src/components/organizations/create/create-organization-form.tsx
+++ frontend/src/components/organizations/create/create-organization-form.tsx
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2025 Copyright AGNTCY Contributors (https://github.com/agntcy)
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import PlaceholderPageContent from '@/components/ui/placeholder-page-content';
 
 export const CreateOrganizationForm = () => {
--- frontend/src/components/ui/confirm-modal.tsx
+++ frontend/src/components/ui/confirm-modal.tsx
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2025 Copyright AGNTCY Contributors (https://github.com/agntcy)
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import React, {ReactNode} from 'react';
 import {Modal, ModalTitle, ModalContent, ModalActions, Button, Typography} from '@outshift/spark-design';
 
