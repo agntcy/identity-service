9ca57bdbab0d1e4f7c1e026c8d2e7139ff6b3cd6
Rafael Silva (rafaelsi) <rafaelsi@cisco.com>
2025-06-26 11:16:47 +0100

    fix: spaces dashboard

--- frontend/src/pages/dashboard/dashboard.tsx
+++ frontend/src/pages/dashboard/dashboard.tsx
@@ -22,8 +22,8 @@ const Dashboard: React.FC = () => {
   }, [navigate]);
 
   return (
-    <ScrollShadowWrapper className="flex flex-col gap-4">
-      <div className="pb-4">
+    <ScrollShadowWrapper>
+      <div className="flex flex-col h-full gap-[24px]">
         <div className="w-full h-[184px] bg-[#00142B] flex flex-col justify-between">
           <div className="flex justify-center items-center my-auto">
             <div>
@@ -37,8 +37,8 @@ const Dashboard: React.FC = () => {
           </div>
           <div className="striped-bar" />
         </div>
-        <div className="dashboard-card mx-4 mt-4">
-          <div className="dashboard-card-content flex flex-col items-center justify-center h-full py-4 gap-[12px] px-4">
+        <div className="dashboard-card mx-6 mb-6">
+          <div className="dashboard-card-content flex flex-col items-center justify-center h-full pt-4 gap-[12px]">
             <div>
               <div className="flex justify-center items-center h-full">
                 <img src={EmptyState} alt="Empty State" className="w-[200px] h-[200px]" />
@@ -66,8 +66,8 @@ const Dashboard: React.FC = () => {
                 Learn more in our documentation.
               </Link>
             </div>
-            <div className="flex gap-4 w-full mt-4">
-              <div className="bg-[#FBFCFE] rounded-[8px] pt-[40px] pb-[24px] flex justify-center items-center px-4 w-[50%]">
+            <div className="flex gap-4 w-full px-4 pb-4 mt-4">
+              <div className="bg-[#FBFCFE] rounded-[8px] flex-col flex justify-center items-center px-20 w-[50%] pt-[40px] pb-[24px]">
                 <div>
                   <Typography variant="h6" textAlign="center">
                     Verify identities
@@ -76,14 +76,14 @@ const Dashboard: React.FC = () => {
                     Begin verifying your MCP servers, agents, and A2A identities to ensure secure communication and proper authentication across your
                     environment.
                   </Typography>
-                  <div className="flex justify-center items-center mt-4">
+                  <div className="flex justify-center items-center mt-8">
                     <Button variant="outlined" sx={{fontWeight: '600 !important'}} startIcon={<CheckIcon className="w-4 h-4" />}>
                       Verify identity
                     </Button>
                   </div>
                 </div>
               </div>
-              <div className="bg-[#FBFCFE] rounded-[8px] pt-[40px] pb-[24px] flex justify-center items-center px-4  w-[50%]">
+              <div className="bg-[#FBFCFE] rounded-[8px] flex-col flex justify-center items-center px-20 w-[50%] pt-[40px] pb-[24px]">
                 <div>
                   <Typography variant="h6" textAlign="center">
                     Create identities
@@ -92,7 +92,7 @@ const Dashboard: React.FC = () => {
                     Start adding agents (MCP servers, agents, and A2A) to create and manage identities, apply tool-based access control and policies
                     effectively.
                   </Typography>
-                  <div className="flex justify-center items-center mt-4">
+                  <div className="flex justify-center items-center mt-8">
                     <Button
                       variant="outlined"
                       sx={{fontWeight: '600 !important'}}
