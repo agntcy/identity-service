929414dd670acd000f1ceee95df7b6e9b9727bc9
copasseron <cpassero@cisco.com>
2025-06-30 11:46:42 +0200

    fix: livense
    Signed-off-by: copasseron <cpassero@cisco.com>

--- deployments/docker/frontend/nginx/entrypoint.sh
+++ deployments/docker/frontend/nginx/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Copyright 2025 Copyright AGNTCY Contributors (https://github.com/agntcy)
+# SPDX-License-Identifier: Apache-2.0
+
 
 envsubst '$VITE_APP_CLIENT_PORT $VITE_API_URL $VITE_APP_LOG_LEVEL $VITE_SEGMENT_ID $VITE_NODE_ENV $VITE_IAM_PRODUCT_ID $VITE_IAM_UI $VITE_IAM_API $VITE_IAM_OIDC_CLIENT_ID $VITE_IAM_OIDC_ISSUER' < /home/web/nginx/nginx.env.conf > /home/web/nginx/nginx.conf
 exec "$@"
--- scripts/unit-test.sh
+++ scripts/unit-test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright 2025 Copyright AGNTCY Contributors (https://github.com/agntcy)
+# SPDX-License-Identifier: Apache-2.0
+
 
 # unit-test.sh
 # Placeholder script for unit tests.
