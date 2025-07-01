4d32f5b782a6439b898db540d5a2482f07588a29
copasseron <cpassero@cisco.com>
2025-07-01 10:43:31 +0200

    chore: add license
    Signed-off-by: copasseron <cpassero@cisco.com>

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2025 Copyright AGNTCY Contributors (https://github.com/agntcy)
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 export const encodeBase64 = (input: string): string => {
   return btoa(input);
 };
