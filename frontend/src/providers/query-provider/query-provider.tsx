1c70aadb7be3cf3a85541513b9164cc679a2a0e8
jdiaconu <jdiaconu@cisco.com>
2025-07-03 10:31:32 +0200

    feat: Add back dev
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

--- frontend/src/providers/query-provider/query-provider.tsx
+++ frontend/src/providers/query-provider/query-provider.tsx
@@ -18,6 +18,7 @@ const queryClient = new QueryClient({
 export const QueryProvider = ({children}: React.PropsWithChildren) => {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
       {children}
     </QueryClientProvider>
   );
