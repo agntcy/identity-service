8bbe95acb001bc43c19fcf4d0658bb85c17478aa
Rafael Silva (rafaelsi) <rafaelsi@cisco.com>
2025-07-03 14:45:11 +0100

    fix: update bff

--- frontend/src/pages/settings/identity-provider/settings-create-identity-provider.tsx
+++ frontend/src/pages/settings/identity-provider/settings-create-identity-provider.tsx
@@ -6,11 +6,36 @@
 import {CreateIdentityProvider} from '@/components/identity-provider/create/create-identity-provider';
 import {BasePage} from '@/components/layout/base-page';
 import {PATHS} from '@/router/paths';
+import {IconButton} from '@mui/material';
+import {Tooltip} from '@outshift/spark-design';
+import {InfoIcon} from 'lucide-react';
 
 const SettingsCreateIdentityProvider: React.FC = () => {
   return (
     <BasePage
-      title="Identity Provider Creation"
+      title={
+        <div className="flex items-center gap-2">
+          <span>Identity Provider Creation</span>
+          <Tooltip
+            title={
+              <div className="text-center">
+                In the first release, you can link a single identity provider as an issuer, but it won&apos;t be possible to edit or remove it.
+              </div>
+            }
+            placement="top"
+          >
+            <IconButton
+              sx={(theme) => ({
+                color: theme.palette.vars.baseTextDefault,
+                width: '24px',
+                height: '24px'
+              })}
+            >
+              <InfoIcon className="w-4 h-4" />
+            </IconButton>
+          </Tooltip>
+        </div>
+      }
       breadcrumbs={[
         {
           text: 'Settings',
