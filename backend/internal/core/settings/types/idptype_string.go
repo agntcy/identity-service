1884f7d88f7590eb0c0934aba72479deb891add5
jdiaconu <jdiaconu@cisco.com>
2025-07-03 15:30:38 +0200

    feat: Update types
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

--- backend/internal/core/settings/types/idptype_string.go
+++ backend/internal/core/settings/types/idptype_string.go
@@ -11,12 +11,13 @@ func _() {
 	_ = x[IDP_TYPE_UNSPECIFIED-0]
 	_ = x[IDP_TYPE_DUO-1]
 	_ = x[IDP_TYPE_OKTA-2]
-	_ = x[IDP_TYPE_SELF-3]
+	_ = x[IDP_TYPE_ORY-3]
+	_ = x[IDP_TYPE_SELF-4]
 }
 
-const _IdpType_name = "IDP_TYPE_UNSPECIFIEDIDP_TYPE_DUOIDP_TYPE_OKTAIDP_TYPE_SELF"
+const _IdpType_name = "IDP_TYPE_UNSPECIFIEDIDP_TYPE_DUOIDP_TYPE_OKTAIDP_TYPE_ORYIDP_TYPE_SELF"
 
-var _IdpType_index = [...]uint8{0, 20, 32, 45, 58}
+var _IdpType_index = [...]uint8{0, 20, 32, 45, 57, 70}
 
 func (i IdpType) String() string {
 	if i < 0 || i >= IdpType(len(_IdpType_index)-1) {
