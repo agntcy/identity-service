853ad14e052fa1b0ddaa1545cd579a0551bafd47
jdiaconu <jdiaconu@cisco.com>
2025-07-04 15:45:18 +0200

    feat: Start
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

---
+++ frontend/src/utils/docs.ts
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2025 Copyright AGNTCY Contributors (https://github.com/agntcy)
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export const parseError = (error: unknown) => {
+  if (!error) {
+    return 'Unknown error';
+  }
+  if (typeof error === 'string') {
+    return error;
+  }
+  if (
+    // If API returns {"error": "some error message""}
+    typeof error === 'object' &&
+    (error as any)?.response?.data?.error
+  ) {
+    return (error as any)?.response?.data?.error;
+  }
+  if (
+    // If API returns {"message": "some error message""}
+    typeof error === 'object' &&
+    (error as any)?.response?.data?.message
+  ) {
+    return (error as any)?.response?.data?.message;
+  }
+  if (
+    // API returns a raw string error
+    typeof error === 'object' &&
+    (error as any)?.response?.data
+  ) {
+    return (error as any)?.response?.data;
+  }
+  // If the error is actually a JS error object
+  if (typeof error === 'object' && 'message' in error) {
+    return (error as any).message;
+  }
+  return 'Unknown error';
+};
