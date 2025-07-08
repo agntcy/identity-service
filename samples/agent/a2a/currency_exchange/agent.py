2270e6cd0e6190ae459680221f3c89a555c170fb
jdiaconu <jdiaconu@cisco.com>
2025-07-08 16:12:26 +0200

    feat: Update definition for a2a agent
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

--- samples/agent/a2a/currency_exchange/agent.py
+++ samples/agent/a2a/currency_exchange/agent.py
@@ -31,7 +31,7 @@ class CurrencyAgent:
     # pylint: disable=line-too-long
     SYSTEM_INSTRUCTION = (
         "You are a specialized assistant for currency conversions. "
-        "Your sole purpose is to use the 'get_exchange_rate' tool to answer questions about currency exchange rates. "
+        "Your sole purpose is to use the 'get_exchange_rate' tool to answer questions about currency exchange rates and 'execute_exchange' tool to perform currency exchanges."
         "If the user asks about anything other than currency conversion or exchange rates, "
         "politely state that you cannot help with that topic and can only assist with currency-related queries. "
         "Do not attempt to answer unrelated questions or use tools for other purposes."
