d9e696641d57eccf1ecf34081793763e05c8c820
jdiaconu <jdiaconu@cisco.com>
2025-07-11 15:43:09 +0200

    feat: Add starette A2A support
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

--- sdk/python/identityplatform/auth/starlette.py
+++ sdk/python/identityplatform/auth/starlette.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 """Middleware for Starlette that authenticates the Identity Platform bearer token."""
 
+from a2a.types import AgentCard, HTTPAuthSecurityScheme
 from identityplatform.sdk import IdentityPlatformSdk as Sdk
 
 from starlette.applications import Starlette
@@ -11,7 +12,7 @@ from starlette.responses import JSONResponse, PlainTextResponse
 
 
 class IdentityPlatformMiddleware(BaseHTTPMiddleware):
-    """Starlette middleware that authenticates A2A access using an OAuth2 bearer token."""
+    """Starlette middleware that authenticates access using an OAuth2 bearer token."""
 
     def __init__(
         self,
@@ -73,3 +74,39 @@ class IdentityPlatformMiddleware(BaseHTTPMiddleware):
         return JSONResponse(
             {"error": "unauthorized", "reason": reason}, status_code=401
         )
+
+
+class IdentityPlatformA2AMiddleware(IdentityPlatformMiddleware):
+    """Starlette middleware that authenticates A2A access using an OAuth2 bearer token."""
+
+    def __init__(
+        self,
+        app: Starlette,
+        agent_card: AgentCard | None = None,
+        public_paths: list[str] = [],
+    ):
+        """Initialize the middleware."""
+        super().__init__(app, public_paths)
+        self.agent_card = agent_card
+
+        if self.agent_card is None:
+            raise ValueError(
+                "AgentCard must be provided to IdentityPlatformMiddleware."
+            )
+
+        if self.agent_card.securitySchemes is None:
+            raise ValueError(
+                "AgentCard must have securitySchemes defined for IdentityPlatformMiddleware."
+            )
+
+        # Process the Security Requirements Object to make sure that the IdentityPlatformAuthScheme is used
+        for sec_scheme in self.agent_card.securitySchemes.values():
+            if isinstance(sec_scheme.root, HTTPAuthSecurityScheme):
+                if sec_scheme.root.scheme != "bearer":
+                    raise ValueError(
+                        "IdentityPlatformMiddleware requires a bearer token scheme."
+                    )
+                if sec_scheme.root.bearerFormat != "JWT":
+                    raise ValueError(
+                        "IdentityPlatformMiddleware requires a JWT bearer format."
+                    )
