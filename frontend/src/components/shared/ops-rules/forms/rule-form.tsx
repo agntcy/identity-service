39385a433dda905acc611721d020f84962bffba4
Rafael Silva (rafaelsi) <rafaelsi@cisco.com>
2025-07-18 10:41:03 +0100

    fix: enable devices policies

--- frontend/src/components/shared/ops-rules/forms/rule-form.tsx
+++ frontend/src/components/shared/ops-rules/forms/rule-form.tsx
@@ -6,13 +6,23 @@
 import {useFormContext} from 'react-hook-form';
 import {FormControl, FormField, FormItem, FormLabel} from '@/components/ui/form';
 import {Input} from '@/components/ui/input';
-import {Checkbox} from '@outshift/spark-design';
+import {Checkbox, IconButton, Skeleton} from '@outshift/spark-design';
 import {TaskForm} from './task-form';
 import {RuleFormValues} from '@/schemas/rule-schema';
 import {Policy} from '@/types/api/policy';
+import {InfoIcon} from 'lucide-react';
+import {useGetDevices} from '@/queries';
+import {useMemo} from 'react';
 
 export const RuleForm = ({isLoading = false, policy}: {isLoading?: boolean; policy?: Policy}) => {
   const policyForm = useFormContext<RuleFormValues>();
+
+  const {data: dataDevices, isLoading: isLoadingDevices, isError: isErrorDevices} = useGetDevices();
+
+  const hasDevices = useMemo(() => {
+    return (dataDevices?.devices?.length ?? 0) > 0;
+  }, [dataDevices]);
+
   return (
     <div className="space-y-6 w-full">
       <div className="w-full flex gap-8">
@@ -42,24 +52,39 @@ export const RuleForm = ({isLoading = false, policy}: {isLoading?: boolean; poli
         />
       </div>
       <TaskForm isLoading={isLoading} policy={policy} />
-      <FormField
-        control={policyForm.control}
-        name={'needsApproval'}
-        render={({field}) => (
-          <FormItem className="w-full">
-            <FormControl>
-              <div className="flex items-center">
-                <Checkbox
-                  id="needs-approval-checkbox"
-                  checked={field.value}
-                  onChange={(checked) => field.onChange(checked)}
-                />
-                <FormLabel className="form-label">Needs Approval?</FormLabel>
-              </div>
-            </FormControl>
-          </FormItem>
-        )}
-      />
+      {isLoadingDevices || isErrorDevices ? (
+        <Skeleton sx={{marginTop: '14px', height: '50px', width: '50px'}} />
+      ) : (
+        <FormField
+          control={policyForm.control}
+          name={'needsApproval'}
+          render={({field}) => (
+            <FormItem className="w-full">
+              <FormControl>
+                <div className="flex items-center">
+                  <Checkbox
+                    disabled={!hasDevices}
+                    id="needs-approval-checkbox"
+                    checked={field.value}
+                    onChange={(checked) => field.onChange(checked)}
+                  />
+                  <FormLabel className="form-label">Needs Approval?</FormLabel>
+                  <IconButton
+                    sx={(theme) => ({
+                      color: theme.palette.vars.baseTextDefault,
+                      width: '24px',
+                      height: '24px',
+                      marginLeft: '8px'
+                    })}
+                  >
+                    <InfoIcon className="w-4 h-4" />
+                  </IconButton>
+                </div>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      )}
     </div>
   );
 };
