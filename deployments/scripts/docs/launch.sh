9d45da65cc52aa2eb2464f117ac043ba331ba2a8
Rafael Silva (rafaelsi) <rafaelsi@cisco.com>
2025-08-08 10:25:40 +0100

    feat: add maze docs

--- deployments/scripts/docs/launch.sh
+++ deployments/scripts/docs/launch.sh
@@ -2,5 +2,9 @@
 # Copyright 2025 Cisco Systems, Inc. and its affiliates
 # SPDX-License-Identifier: Apache-2.0
 
+# If .env exists in the webapi directory, use it
+# If not, create an env with defaults
+./deployments/scripts/docs/env_setup.sh
+
 docker compose -f ./deployments/docker-compose/docs/docker-compose.yml build --no-cache
 docker compose -f ./deployments/docker-compose/docs/docker-compose.yml up -d
