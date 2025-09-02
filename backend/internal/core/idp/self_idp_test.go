9a2600df9b0f2a7366de8939320d47511b5f4336
Mohamed Tahar KEDJOUR <mkedjour@cisco.com>
2025-09-02 18:08:04 +0200

    test(idp): added unit tests for idp factory and self idp
    Signed-off-by: Mohamed Tahar KEDJOUR <mkedjour@cisco.com>

---
+++ backend/internal/core/idp/self_idp_test.go
@@ -0,0 +1,29 @@
+// Copyright 2025 Cisco Systems, Inc. and its affiliates
+// SPDX-License-Settingsentifier: Apache-2.0
+
+package idp_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/outshift/identity-service/internal/core/idp"
+	identitycontext "github.com/outshift/identity-service/internal/pkg/context"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSelfIdp_CreateClientCredentialsPair(t *testing.T) {
+	t.Parallel()
+
+	userID := uuid.NewString()
+	ctx := identitycontext.InsertUserID(context.Background(), userID)
+	sut := idp.NewSelfIdp()
+
+	clientCred, err := sut.CreateClientCredentialsPair(ctx)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, clientCred)
+	assert.Equal(t, userID, clientCred.Issuer)
+	assert.NotEmpty(t, clientCred.ClientID)
+}
