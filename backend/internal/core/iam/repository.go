ad0ba49db1d04f45d6b0e04b524c9d0557171f22
jdiaconu <jdiaconu@cisco.com>
2025-09-02 14:55:12 +0200

    feat: Add repository
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

---
+++ backend/internal/core/iam/repository.go
@@ -0,0 +1,22 @@
+// Copyright 2025 Cisco Systems, Inc. and its affiliates
+// SPDX-License-Identifier: Apache-2.0
+
+package APIKey
+
+import (
+	"context"
+
+	"github.com/outshift/identity-service/internal/core/iam/types"
+)
+
+type Repository interface {
+	AddAPIKey(
+		ctx context.Context,
+		APIKey *types.APIKey,
+	) (*types.APIKey, error)
+	GetAPIKey(
+		ctx context.Context,
+		APIKeyID string,
+	) (*types.APIKey, error)
+	DeleteAPIKey(ctx context.Context, APIKey *types.APIKey) error
+}
