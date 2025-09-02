acd1e09662e6b1918cc72ed145e2cf22b09b6e96
jdiaconu <jdiaconu@cisco.com>
2025-09-02 17:03:38 +0200

    feat: Update iam interface
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

--- backend/internal/pkg/iam/iam.go
+++ backend/internal/pkg/iam/iam.go
@@ -16,15 +16,14 @@ type Client interface {
 	) (newCtx context.Context, err error)
 	AuthAPIKey(
 		ctx context.Context,
-		productID string,
 		apiKey string,
 		forApp bool,
 	) (newCtx context.Context, err error)
-	GetTenantAPIKey(ctx context.Context) (apiKey types.APIKey, err error)
-	GetAppAPIKey(ctx context.Context, appID string) (apiKey types.APIKey, err error)
-	RefreshAppAPIKey(ctx context.Context, appID string) (apiKey types.APIKey, err error)
-	CreateTenantAPIKey(ctx context.Context) (apiKey types.APIKey, err error)
-	CreateAppAPIKey(ctx context.Context, appID string) (apiKey types.APIKey, err error)
+	GetTenantAPIKey(ctx context.Context) (apiKey *types.APIKey, err error)
+	GetAppAPIKey(ctx context.Context, appID string) (apiKey *types.APIKey, err error)
+	RefreshAppAPIKey(ctx context.Context, appID string) (apiKey *types.APIKey, err error)
+	CreateTenantAPIKey(ctx context.Context) (apiKey *types.APIKey, err error)
+	CreateAppAPIKey(ctx context.Context, appID string) (apiKey *types.APIKey, err error)
 	RevokeTenantAPIKey(ctx context.Context) (err error)
 	RevokeAppAPIKey(ctx context.Context, appID string) (err error)
 }
