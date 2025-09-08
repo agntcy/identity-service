d7d5715f135d622535a21aee046e7ddbad886fa3
jdiaconu <jdiaconu@cisco.com>
2025-09-08 09:37:18 +0200

    feat: Update flag
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

--- backend/internal/pkg/iam/iam_multitenant_public.go
+++ backend/internal/pkg/iam/iam_multitenant_public.go
@@ -1,5 +1,5 @@
-//go:build !outshiftLM
-// +build !outshiftLM
+//go:build !outshift
+// +build !outshift
 
 // Copyright 2025 Copyright AGNTCY Contributors (https://github.com/agntcy)
 // SPDX-License-Identifier: Apache-2.0
