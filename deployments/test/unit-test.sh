6a0545d0cc12c69ad3967429596a7e9ec9aa22de
jdiaconu <jdiaconu@cisco.com>
2025-09-15 09:45:25 +0200

    feat: Add tests for UI
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

--- deployments/test/unit-test.sh
+++ deployments/test/unit-test.sh
@@ -2,9 +2,14 @@
 # Copyright 2025 Cisco Systems, Inc. and its affiliates
 # SPDX-License-Identifier: Apache-2.0
 
+DOCKER_FILE=./deployments/docker/frontend/Dockerfile.test
+TEST_COMMAND='npx vitest . --browser.headless=true'
+
+echo RUNNING FRONTEND TESTS
+docker run "$(docker build --no-cache -f ${DOCKER_FILE} -q .)" $TEST_COMMAND
 
 DOCKER_FILE=./deployments/docker/backend/Dockerfile.test
 TEST_COMMAND='go test -cover -v ./...'
 
-echo RUNNING TESTS
+echo RUNNING BACKEND TESTS
 docker run "$(docker build --no-cache -f ${DOCKER_FILE} -q .)" $TEST_COMMAND
--- scripts/unit-test.sh
+++ scripts/unit-test.sh
@@ -2,10 +2,4 @@
 # Copyright 2025 Copyright AGNTCY Contributors (https://github.com/agntcy)
 # SPDX-License-Identifier: Apache-2.0
 
-
-# unit-test.sh
-# Placeholder script for unit tests.
-# Implementation coming soon.
-
-echo "Unit tests are not implemented yet."
-exit 0
+./deployments/test/unit-test.sh
