1e9a63b133a286a5fd39168be2ca8e22f16307b3
Mohamed Tahar KEDJOUR <mkedjour@cisco.com>
2025-09-25 18:03:45 +0200

    chore(gosdk): changed the go module path
    Signed-off-by: Mohamed Tahar KEDJOUR <mkedjour@cisco.com>

--- backend/api/client/client/badge/issue_badge_parameters.go
+++ backend/api/client/client/badge/issue_badge_parameters.go
@@ -15,7 +15,7 @@ import (
 	cr "github.com/go-openapi/runtime/client"
 	"github.com/go-openapi/strfmt"
 
-	"github.com/outshift/identity-service/api/client/models"
+	"github.com/outshift-open/identity-service/backend/api/client/models"
 )
 
 // NewIssueBadgeParams creates a new IssueBadgeParams object,
