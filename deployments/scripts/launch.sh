993861fbab67700a4aa892e7721aa9c387408f40
jdiaconu <jdiaconu@cisco.com>
2025-09-25 19:07:48 +0200

    feat: Add global launch & stop scripts
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

---
+++ deployments/scripts/launch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Copyright 2025 Cisco Systems, Inc. and its affiliates
+# SPDX-License-Identifier: Apache-2.0
+
+./deployments/scripts/backend/launch.sh
+./deployments/scripts/frontend/launch.sh
+./deployments/scripts/docs/launch.sh
---
+++ deployments/scripts/stop.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Copyright 2025 Cisco Systems, Inc. and its affiliates
+# SPDX-License-Identifier: Apache-2.0
+
+./deployments/scripts/backend/stop.sh
+./deployments/scripts/frontend/stop.sh
+./deployments/scripts/docs/stop.sh
