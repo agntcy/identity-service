9bc08b568d0b47eeeace117eddb31fe1a2ae931f
Rafael Silva (rafaelsi) <rafaelsi@cisco.com>
2025-09-26 10:59:52 +0100

    feat: integrate oidc provider
    Signed-off-by: Rafael Silva (rafaelsi) <rafaelsi@cisco.com>

--- deployments/docker/frontend/nginx/entrypoint.sh
+++ deployments/docker/frontend/nginx/entrypoint.sh
@@ -4,5 +4,5 @@
 
 
 envsubst '$VITE_APP_CLIENT_PORT $VITE_API_URL $VITE_APP_LOG_LEVEL $VITE_SEGMENT_ID $VITE_NODE_ENV $VITE_IAM_PRODUCT_ID $VITE_IAM_UI $VITE_IAM_API $VITE_IAM_OIDC_CLIENT_ID $VITE_IAM_OIDC_ISSUER $VITE_AUTH_TYPE $VITE_OIDC_UI $VITE_OIDC_CLIENT_ID $VITE_OIDC_ISSUER $VITE_IAM_MULTI_TENANT $VITE_DOCS_URL $VITE_MAZE_ID $VITE_APP_BASE_NAME' < /home/web/nginx/nginx.env.conf > /home/web/nginx/nginx.conf
-envsubst '$VITE_OIDC_UI $VITE_API_URL' < /home/web/nginx/csp-header.env.conf > /home/web/nginx/csp-header.conf
+envsubst '$VITE_OIDC_UI $VITE_OIDC_ISSUER $VITE_API_URL' < /home/web/nginx/csp-header.env.conf > /home/web/nginx/csp-header.conf
 exec "$@"
