b07623f3d75fc5ce87604524a195e461261da1a3
Rafael Silva (rafaelsi) <rafaelsi@cisco.com>
2025-10-29 08:55:48 +0000

    feat: update secret IDp UI
    Signed-off-by: Rafael Silva (rafaelsi) <rafaelsi@cisco.com>

--- frontend/src/api/generated/identity/app.swagger.api.ts
+++ frontend/src/api/generated/identity/app.swagger.api.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2025 AGNTCY Contributors (https://github.com/agntcy)
+ * Copyright 2025 Copyright AGNTCY Contributors (https://github.com/agntcy)
  * SPDX-License-Identifier: Apache-2.0
  */
 
